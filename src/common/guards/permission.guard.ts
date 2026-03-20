8ec07b8b082f2794f4e4b39d87cee1c168355e7e
irzix <ialirezamhi@gmail.com>
2026-03-20 18:03:29 +0200

    chore: Remove console.log

--- src/common/guards/permission.guard.ts
+++ src/common/guards/permission.guard.ts
@@ -29,8 +29,6 @@ export class PermissionsGuard implements CanActivate {
 
     if (!role) throw new ForbiddenException();
 
-    console.log(role.permissions);
-
     const has = role.permissions.some(
       (p) =>
         p.resource === required.resource &&
